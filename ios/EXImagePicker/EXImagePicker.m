3f943d3add434fbb4d8c443b335e27288b54a872
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2019-07-11 12:58:25 +0200

    [image-picker] Add support for video recording via `launchCameraAsync` (#4903)
    Resolve #4310.
    
    Added `mediaType` support for Android.
    
    You can check it on https://snack.expo.io/@lukaszkosmaty/874a3d

--- ios/EXImagePicker/EXImagePicker.m
+++ ios/EXImagePicker/EXImagePicker.m
@@ -303,7 +303,6 @@ UM_EXPORT_METHOD_AS(launchImageLibraryAsync, launchImageLibraryAsync:(NSDictiona
   } else {
     UMLogInfo(@"Could not fetch metadata for video %@", [videoURL absoluteString]);
   }
-
   if (([[self.options objectForKey:@"allowsEditing"] boolValue])) {
     AVURLAsset *editedAsset = [AVURLAsset assetWithURL:videoURL];
     CMTime duration = [editedAsset duration];
@@ -328,6 +327,18 @@ UM_EXPORT_METHOD_AS(launchImageLibraryAsync, launchImageLibraryAsync:(NSDictiona
 
   NSURL *fileURL = [NSURL fileURLWithPath:path];
   NSString *filePath = [fileURL absoluteString];
+
+  // adding data to response if video came from camera
+  if (!info[UIImagePickerControllerReferenceURL]) {
+    AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:fileURL options:nil];
+    CGSize size = [[[asset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0] naturalSize];
+    response[@"width"] = @(size.width);
+    response[@"height"] = @(size.height);
+    if (!response[@"duration"]) {
+      CMTime duration = [asset duration];
+      response[@"duration"] = @(ceil((float) duration.value / duration.timescale * 1000));
+    }
+  }
   response[@"uri"] = filePath;
 }
 
